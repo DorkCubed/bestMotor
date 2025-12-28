566348987e8300f723474cac74aad1ccc6c355d4
unknown <f20231087@pilani.bits-pilani.ac.in>
2025-12-28 18:45:10 +0530

    minor fixes to reduce evaluations

--- run.py
+++ run.py
@@ -29,22 +29,28 @@ def arr_to_props(arr) -> Motorprops:
     props.exit = float(arr[7])
     return props
 
-def random_scale(i: int, it: int, scale=0.1):
-    return (1 + random.uniform(-1,1) * scale) * (1 - (i / it))
+def random_scale(i: int, it: int, scale=0.01):
+    return (1 + random.uniform(-10,10) * scale) * (1 - (i / it))
 
 def anneal_train(finalfile, startprops: Motorprops, iterations=100, angle_lock = True):
+    with open(finalfile, "a") as fin:
+        writeall(["throat", "len", "number of grains", "grain diameter", "core diameter", "convergance angle",
+             "divergance angle", "burn time", "thrust", "avg thrust", "peak pressure", "flatness"], fin, True)
+    fin.close()
+    
     oldarr = props_to_arr(startprops) 
     newarr = []
     old = eval(finalfile, arr_to_props(oldarr), "output")
 
     for i in range(iterations):
-        for j in range(len(newarr)):
+        for j in range(len(oldarr)):
             if j == 0:
                 continue
             if angle_lock and (j == 4 or j == 5):
                 continue
-            
+        
             newarr = oldarr.copy()
+
             if (j == 1 or j == 4 or j == 5):
                 newarr[j] = round(newarr[j] * random_scale(i, iterations, 1))
             else:
@@ -62,6 +68,9 @@ def anneal_train(finalfile, startprops: Motorprops, iterations=100, angle_lock =
                 case 7:
                     throat = sqrt(sq(newarr[3]) / 3.05)
                     if (newarr[7] != bounded(newarr[7], throat * 1.1, 0.5)): continue
+
+            if newarr[j] == oldarr[j]:
+                continue
 
             new = eval(finalfile, arr_to_props(newarr), "output")
             if new > old:
@@ -81,4 +90,5 @@ props.div_angle = 12
 props.throatlen = 0.0
 props.prop_weight = 1000
 props.exit = 0.1
+
 anneal_train("outputs.csv", props)
